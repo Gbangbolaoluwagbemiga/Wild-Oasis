62414a09a453f9f79dfcca57bc89865e5b3ae7a9
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2024-12-03 17:23:30 +0100

    init

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
+import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
-import FormRowVertical from "../../ui/FormRowVertical";
 
 function LoginForm() {
   const [email, setEmail] = useState("");
@@ -12,13 +12,17 @@ function LoginForm() {
 
   return (
     <Form onSubmit={handleSubmit}>
-      <FormRowVertical label="Email address">
+      <FormRow label="Email address" orientation="vertical">
         <Input
           type="email"
           id="email"
           // This makes this form better for password managers
-          autoComplete="username"     value={email}       onChange={(e) => setEmail(e.target.value)}/FormRowVertical>
-      <FormRowVertical label="Password">
+          autoComplete="username"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormRow>
+      <FormRow label="Password" orientation="vertical">
         <Input
           type="password"
           id="password"
@@ -26,10 +30,10 @@ function LoginForm() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-      </FormRowVertical>
-      <FormRowVertical>
+      </FormRow>
+      <FormRow orientation="vertical">
         <Button size="large">Login</Button>
-      </FormRowVertical>
+      </FormRow>
     </Form>
   );
 }
