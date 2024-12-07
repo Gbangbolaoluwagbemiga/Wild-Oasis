3d95c1a391684736b091d32cfa3a498f4338f115
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2024-12-07 16:30:37 +0100

    Table init

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,8 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import styled from "styled-components";
 import { getCabins } from "../../services/apiCabins";
-import Spinner from "../../ui/Spinner"; // v2
+import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
+
+const Table = styled.div`
+  border: 1px solid var(--color-grey-200);
+
+  font-size: 1.4rem;
+  background-color: var(--color-grey-0);
+  border-radius: 7px;
+  overflow: hidden;
+`;
 
 const TableHeader = styled.header`
   display: grid;
@@ -24,11 +33,15 @@ function CabinTable() {
     isLoading,
     data: cabins,
     error,
-  } = useQuery({ queryKey: ["cabin"], queryFn: getCabins });
+  } = useQuery({
+    queryKey: ["cabins"],
+    queryFn: getCabins,
+  });
 
   if (isLoading) return <Spinner />;
+
   return (
-    <>
+    <Table role="table">
       <TableHeader role="row">
         <div></div>
         <div>Cabin</div>
@@ -39,8 +52,8 @@ function CabinTable() {
       </TableHeader>
       {cabins.map((cabin) => (
         <CabinRow cabin={cabin} key={cabin.id} />
-      ))}{" "}
-    </>
+      ))}
+    </Table>
   );
 }
 
