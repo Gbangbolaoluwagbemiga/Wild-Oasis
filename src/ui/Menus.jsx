354cb44a431f329c7ac44318258a43ed2f28f62d
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2025-01-08 23:17:18 +0100

    init

--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -67,7 +67,7 @@ const StyledButton = styled.button`
 
 const MenusContext = createContext();
 
-function Menus({ children }) {
+function Menus({ childre }) {
   const [openId, setOpen] = useState("");
   const close = () => setOpen("");
   const open = setOpen;
